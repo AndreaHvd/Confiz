411224f731241f72fe5b50c802e29c665926626e
AndreaH <94178978+Kayak512@users.noreply.github.com>
2022-06-14 15:32:25 +0200

    update index.php

--- head.php
+++ head.php
@@ -3,8 +3,12 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="components.css">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Island+Moments&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300&family=Island+Moments&display=swap" rel="stylesheet">
         <title>Confiz</title>
     </head>
--- index.php
+++ index.php
@@ -2,6 +2,21 @@
 include_once("head.php");
 ?>
     <body class="login">
-        <p>Confiz</p>
+        <h1 class="titre">Confiz</h1>
+        <div class="rectangle_marron">
+            <div class="rectangle_connexion">
+                <div class="bloc_connexion">
+                    <p class="texte">Nom:</p>
+                    <textarea class="connexion" name="nom"></textarea>
+                </div>
+                <div class="bloc_connexion">
+                    <p class="texte">Mot de passe:</p>
+                    <textarea class="connexion" name="mdp"></textarea>
+                </div>
+            </div>
+            <div class="bouton_connexion">
+                Connexion
+            </div>
+        </div>
     </body>
 </html>
