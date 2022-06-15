3f1e24eb06aa609e713e1fe0fd943a9a2acec79b
AndreaH <94178978+Kayak512@users.noreply.github.com>
2022-06-15 16:54:32 +0200

    modification des components button en input de type submit

--- boutique-client.php
+++ boutique-client.php
@@ -5,9 +5,7 @@ include_once("head.php");
 <body>
     <header>
         <div class="bouton_retour_acceuil">Confiz</div>
-        <div class="barre_recherche">  
-            <input class="barre_recherche" type="text" placeholder="Rechercher une boutique">
-        </div>
+        <input class="barre_recherche" type="text" placeholder="Rechercher une boutique">
         <input type="submit" class="bouton bouton-blanc texte" value="Deconnexion">
     </header>
 </body>
