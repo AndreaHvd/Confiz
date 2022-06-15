3847cf8c58d1bbb35a6d7e893d5b4de680077977
AndreaH <94178978+Kayak512@users.noreply.github.com>
2022-06-15 16:49:30 +0200

    update boutique-client.php

--- boutique-client.php
+++ boutique-client.php
@@ -3,11 +3,13 @@ include_once("head.php");
 ?>
 
 <body>
-    <div class="boutique-client">
-        <div>
-
+    <header>
+        <div class="bouton_retour_acceuil">Confiz</div>
+        <div class="barre_recherche">  
+            <input class="barre_recherche" type="text" placeholder="Rechercher une boutique">
         </div>
-    </div>
+        <input type="submit" class="bouton bouton-blanc texte" value="Deconnexion">
+    </header>
 </body>
 
 </html>
--- index.php
+++ index.php
@@ -3,24 +3,18 @@ include_once("head.php");
 ?>
     <body class="login">
         <h1 class="titre">Confiz</h1>
-        <div class="rectangle_marron">
-            <div class="rectangle_connexion">
-                <div class="bloc_connexion">
-                    <p class="texte">Nom:</p>
-                    <form class="connexion" name="nom">
-                        <input class="connexion" type="text">
-                    </form>
-                </div>
-                <div class="bloc_connexion">
-                    <p class="texte">Mot de passe:</p>
-                    <form class="connexion" name="mdp">
-                        <input class="connexion" type="text">
-                    </form>
-                </div>
+        <form class="rectangle_marron" name="connexion" action="" method="">
+        <div class="rectangle_connexion">
+            <div class="bloc_connexion">
+                <p class="texte">Nom:</p>
+                <input class="connexion" type="text">
             </div>
-            <button class="bouton bouton-orange texte">
-                Connexion
-            </button>
+            <div class="bloc_connexion">
+                <p class="texte">Mot de passe:</p>
+                <input class="connexion" type="text">
+            </div>
         </div>
+        <input type="submit" class="bouton bouton-orange texte" value="Connexion">
+        </form>
     </body>
 </html>
