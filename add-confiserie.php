655a14dd19445f5e8069219081315eb872d503d8
mtregoat <106659357+mtregoat@users.noreply.github.com>
2022-06-16 09:44:38 +0200

    add confiserie

--- add-confiserie.php
+++ add-confiserie.php
@@ -5,7 +5,23 @@ include_once("header.php");
 ?>
 
 <body>
-    
+<div class="main-container">
+<div class="add-boutique texte">
+        <p>Nouvelle confiserie</p>
+        <div class="bloc-add">
+            <p>Nom : </p>
+            <input class="zone-texte" type="text">
+        </div>
+        <div class="bloc-add">
+            <p>Prix : </p>
+            <input class="zone-texte" type="text">
+        </div>
+        <div class="bouton-selection">
+            <input type="button" value="Continuer" class="bouton_rectangle texte">
+            <input type="button" value="Annuler" class="bouton_rectangle texte">
+        </div>
+    </div>
+</div>
 </body>
 
 </html>
