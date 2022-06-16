c87e35d40e126824d5ae6b749f35341e84329cbc
AndreaH <94178978+Kayak512@users.noreply.github.com>
2022-06-16 09:33:41 +0200

    update add-boutique.php

--- add-boutique.php
+++ add-boutique.php
@@ -5,19 +5,21 @@ include_once("header.php");
 ?>
 
 <body>
-    <div class="add-boutique texte">
-        <p>Nouvelle Boutique</p>
-        <div class="bloc-add">
-            <p>Nom : </p>
-            <input class="zone-texte" type="text">
-        </div>
-        <div class="bloc-add">
-            <p>Adresse : </p>
-            <input class="zone-texte" type="text">
-        </div>
-        <div class="bouton-selection">
-            <input type="button" value="Continuer" class="bouton_rectangle texte">
-            <input type="button" value="Annuler" class="bouton_rectangle texte">
+    <div class="main-container">
+        <div class="add-boutique texte">
+            <p>Nouvelle Boutique</p>
+            <div class="bloc-add">
+                <p>Nom : </p>
+                <input class="zone-texte" type="text">
+            </div>
+            <div class="bloc-add">
+                <p>Adresse : </p>
+                <input class="zone-texte" type="text">
+            </div>
+            <div class="bouton-selection">
+                <input type="button" value="Continuer" class="bouton_rectangle texte">
+                <input type="button" value="Annuler" class="bouton_rectangle texte">
+            </div>
         </div>
     </div>
 </body>
