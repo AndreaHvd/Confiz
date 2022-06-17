e7e8b657f4b217c741b87f4d32ca05de9ac720f6
AndreaH <94178978+Kayak512@users.noreply.github.com>
2022-06-17 14:36:09 +0200

    page boutique-client.php terminer

--- confiserie-client.php
+++ confiserie-client.php
@@ -5,7 +5,11 @@ include_once("header.php");
 ?>
 
 <body>
-    
+    <?php
+    $id = $_GET['id'];
+    $sql = "SELECT * FROM stocks WHERE boutique_id='$id'";
+    $confiseries = db_query($sql);
+    ?>
 </body>
 
 </html>
