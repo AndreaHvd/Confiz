a4b88ed72c3a2dd41a6ee0bdf63938d24ffb380b
mtregoat <106659357+mtregoat@users.noreply.github.com>
2022-06-17 15:22:30 +0200

    Update boutique-gerant.php

--- boutique-gerant.php
+++ boutique-gerant.php
@@ -5,7 +5,27 @@ include_once("header.php");
 ?>
 
 <body>
-    
+<?php 
+$sql = "SELECT * FROM boutique NATURAL JOIN adresses";
+    $boutique = db_query($sql);
+    foreach($boutique as $element) {
+    echo ("<div class='boutique-gerant texte'>
+    <div class='img-confiserie'>
+        <img src='https://via.placeholder.com/250x200' alt='boutique'>
+    </div>
+    <div class='info-boutique'>
+        <div class='info'>
+            <p>".$element['nom']."</p>
+        </div>
+        <div class='info'>
+            <p>".$element['numero_rue'].$element['nom_adresse'].$element['code_postal'].$element['ville'].$element['pays']."</p>
+        </div>
+    </div>
+</div>");
+    }
+?>
+
+    <input type="button" value="Ajouter une boutique" class="bouton_rectangle">
 </body>
 
 </html>
