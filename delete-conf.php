17622ee841050ab9eb75cf36c3983ce9ca27757c
AndreaH <94178978+AndreaHvd@users.noreply.github.com>
2022-06-22 11:45:42 +0200

    Update delete-conf.php

--- delete-conf.php
+++ delete-conf.php
@@ -1,6 +1,7 @@
 <?php
 include_once("head.php");
 $id = $_GET['id'];
+$var = $_GET['var'];
 $sql = "DELETE FROM stocks WHERE confiserie_id=$var";
 db_query($sql);
 header("location: confiserie-gerant.php?id=$id");
