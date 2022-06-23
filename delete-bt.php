eb6d18a8dfe9fe325e1167c8fadaa076636d2d1e
AndreaH <94178978+AndreaHvd@users.noreply.github.com>
2022-06-23 08:37:22 +0200

    Update delete-bt.php

--- delete-bt.php
+++ delete-bt.php
@@ -1,9 +1,11 @@
 <?php
 include_once("head.php");
 $id = $_GET['id'];
-$sql = "DELETE FROM adresses WHERE boutique_id=$id";
-db_query($sql);
+$sql = "SELECT * FROM boutiques WHERE boutique_id=$id";
+$adresse_id = db_query($sql);
 $sql2 = "DELETE FROM boutiques WHERE boutique_id=$id";
 db_query($sql2);
+$sql3 = "DELETE FROM adresses WHERE adresse_id=".$adresse_id[0]['adresse_id'];
+db_query($sql3);
 header("location: boutique-gerant.php");
 ?>
