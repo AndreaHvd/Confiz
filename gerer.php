2ed0f690d2c75d10f07f0097136ccae1cb538751
AndreaH <94178978+AndreaHvd@users.noreply.github.com>
2022-06-24 15:55:41 +0200

    Update gerer.php

--- gerer.php
+++ gerer.php
@@ -9,11 +9,16 @@ $id = $_GET['id'];
     <?php 
         $sql = "SELECT * FROM utilisateurs WHERE id=$id";
         $user = db_query($sql);
+        
         if($user[0]["type"] == "client"){
-            $sql2 = "UPDATE utilisateurs"
+            $sql2 = "UPDATE utilisateurs SET type = 'gerant' WHERE id=$id";
+            db_query($sql2);
             header("location: gestion-utilisateur.php");
-        } elseif($user[0]["type"] == "gerant"){
-            $user[0]["type"] = "client";
+        } 
+        
+        if($user[0]["type"] == "gerant"){
+            $sql2 = "UPDATE utilisateurs SET type = 'client' WHERE id=$id";
+            db_query($sql3);
             header("location: gestion-utilisateur.php");
         }
     ?>
