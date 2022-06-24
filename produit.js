7525d66eec2eeeb59579b39296970b9cc86503d2
mtregoat <106659357+mtregoat@users.noreply.github.com>
2022-06-24 16:44:19 +0200

    panier

--- produit.js
+++ produit.js
@@ -1,7 +1,37 @@
-function saveproduit(produit){
-    localStorage.setItem("produit", produit);
+function saveconfiserie(confiserie){
+    localStorage.setItem("produit", JSON.stringify (confiserie));
 }
 
-function getproduit(){
-    return localStorage.getItem("produit");
+function getconfiserie(){
+    let confiserie = localStorage.getItem("confiserie");
+    if(confiserie == null){
+        return[];
+    } else{
+        return JSON.parse(confiserie);
+    }
+}
+
+function addproduits(produit){
+    let confiserie = getconfiserie();
+    let foundproduit = confiserie.find(p => p.id == produit.id);
+    if(foundproduit != undefined){
+        foundproduit.quantity++;
+    } else{
+        produit.quantity = 1;
+        confiserie.push(produit);
+    }
+    saveconfiserie(confiserie);
+}
+function removeFromconfiserie(produit){
+    let confiserie = getconfiserie();
+    confiserie = confiserie.filter(p => p.id == produit.id);
+    saveconfiserie(confiserie);
+}
+function changeQuantity(produit, quantity){
+    let confiserie = getconfiserie();
+    let foundproduit = confiserie.find(p => p.id == produit.id);
+    if(foundproduit != undefined){
+        foundproduit.quantity += quantity;
+    }
+    saveconfiserie(confiserie);
 }
