a50e6e9fda550d0d459780ec5f8feacd32789836
mtregoat <106659357+mtregoat@users.noreply.github.com>
2022-06-30 08:36:11 +0200

    icone onglet

--- head.php
+++ head.php
@@ -6,6 +6,8 @@
         ?>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="icon" type="image/x-icon" href="img/icone.ico"/>
+
         <link rel="stylesheet" href="style.css">
         <link rel="stylesheet" href="components.css">
         <link rel="preconnect" href="https://fonts.googleapis.com">
